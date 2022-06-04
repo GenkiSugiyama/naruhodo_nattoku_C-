37191c73e364249633015b1a012c7de24754cbd1
GenkiSugiyama <sgym0318marcos@gmail.com>
2022-06-04 17:03:40 +0900

    第9章確認問題

--- SampleClass/SampleClass/Program.cs
+++ SampleClass/SampleClass/Program.cs
@@ -7,15 +7,49 @@ namespace SampleClass
     {
         public static void Main(string[] args)
         {
-            var lines = new string[]
-            {
-                "祇園精舎の鐘の声、諸行無常の響きあり",
-                "沙羅双樹の花の色、盛者必衰の理をあらはす",
-                "おごれる人も久しからず、ただ春の夜の夢のごとし",
-                "たけき者もついには滅びぬ、ひとへに風の前の塵に同じ"
-            };
+            // OutputMin();
+            // OutputRounding();
+            // FebruaryCount();
+            OutputDayOfWeek();
+        }
 
-            File.WriteAllLines(@"/Users/sugiyamagenki/desktop", lines);
+        private static void OutputMin()
+        {
+            Console.WriteLine("比較対象1の数値を入力してください");
+            var int1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("比較対象2の数値を入力してください");
+            var int2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"小さい方の値は {Math.Min(int1, int2)} です。");
+        }
+
+        private static void OutputRounding()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                
+                if (input == "")
+                {
+                    break;
+                }
+                var double1 = double.Parse(input);
+                var floorValue = Math.Floor(double1);
+                Console.WriteLine($"切り捨て後の値は {floorValue} です");
+                var ceilingValue = Math.Ceiling(double1);
+                Console.WriteLine($"切り上げ後の値は {ceilingValue} です");
+            }
+        }
+
+        private static void FebruaryCount()
+        {
+            var countDays = DateTime.DaysInMonth(2020, 2);
+            Console.WriteLine($"2020年2月の日数は {countDays} です。");
+        }
+
+        private static void OutputDayOfWeek()
+        {
+            var date = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine($"{date} の曜日は {date.DayOfWeek} です");
         }
     }
 }
